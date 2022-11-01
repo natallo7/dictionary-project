9d006dbee1f26d1a1b67dd9afc892109c81a789a
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-11-01 21:24:47 +0100

    Added GitHub repository link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Natalia</small>
+          <small>
+            Coded by{" "}
+            <a
+              href="https://github.com/natallo7/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Natalia Lloyd and open-sourced on GitHub.
+            </a>
+          </small>
         </footer>
       </div>
     </div>
