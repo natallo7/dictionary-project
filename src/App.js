2ebadc063222b8e00d11afbe6ed80ccf492d94ec
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-11-01 16:49:43 +0100

    Created search form and function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Natalia</small>
+        </footer>
+      </div>
     </div>
   );
 }
