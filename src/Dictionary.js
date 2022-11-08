f5214393367ecbb7de94f7b87a39a60241e33c97
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-11-08 21:10:18 +0100

    Added basic styling to sections

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,9 +26,12 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
+      <section>
+        <form onSubmit={search}>
+          <input type="search" onChange={handleKeywordChange} />
+        </form>
+      </section>
+      <div className="hint">Suggested words: sunset, coffee, books, hiking</div>
       <Results results={results} />
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -6,14 +6,16 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
+        <section>
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <section key={index}>
+                <Phonetic phonetic={phonetic} />
+              </section>
+            );
+          })}
+        </section>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
