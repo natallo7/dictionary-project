b23d621b13d07422201817b348f6b6cb8de54d2c
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-12-07 21:41:41 +0100

    Updated styling

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Dictionary</h1>
-        </header>
+        <header className="App-header">Dictionary</header>
         <main>
           <Dictionary defaultKeyword="coffee" />
         </main>
         <footer className="App-footer">
           <small>
-            Coded by Natalia Lloyd{" "}
+            Coded by{" "}
+            <a
+              href="https://natalialloyd.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Natalia Lloyd{" "}
+            </a>
+            and{" "}
             <a
               href="https://github.com/natallo7/dictionary-project"
               target="_blank"
               rel="noopener noreferrer"
             >
-              and open-sourced on GitHub.
+              open-sourced on GitHub.
             </a>
             <br />
             <a href="https://www.freepik.com/free-vector/botanical-themed-design-space_4122027.htm#query=plants%20illustration&position=30&from_view=search&track=sph">
